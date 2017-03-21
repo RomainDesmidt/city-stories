c6550a2857afe9656a67c7ca930efb1b12075195
Romain DESMIDT <romain.desmidt@gmail.com>
2017-03-21 11:27:04 +0100

    routes & namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :journey, only: [:index, :show] do
+    resources :poi, only: [:index, :show]
+  end
+  namespace :artist do
+    resources :journey do
+      resources :poi
+    end
+  end
+  get 'styleguide', to: 'pages#styleguide'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
