079edd49f9eabc2f3f3a13bf80dff282b7376642
Romain DESMIDT <romain.desmidt@gmail.com>
2017-03-25 13:24:17 +0100

    java - ruby

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,5 +1,16 @@
 class JourneysController < ApplicationController
   def index
+    @coordinates_hash = []
+    @journeys = Journey.all
+    @journeys.each do |journey|
+    @coordinates_array = []
+      journey.pois.each do |poi|
+        latitude = poi.latitude
+        longitude = poi.longitude
+        @coordinates_array << [latitude , longitude]
+      end
+      @coordinates_hash << @coordinates_array
+    end
   end
 
   def show
