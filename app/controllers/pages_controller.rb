235167bf622589aa1c17adf1ae32c16dd26027a7
Romain DESMIDT <romain.desmidt@gmail.com>
2017-03-27 13:13:43 +0200

    comportement ajoute

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,9 +13,32 @@ class PagesController < ApplicationController
 
 
   def value
-    @poi = POI.all
-    @a = {}
-    @a = {lato: params[:pos], c: "d"}
-    render :json => @a
+    @result = []
+    # POI name for user
+    # Journey name for user
+    # Poi content
+    # User avatar
+
+
+    POI.where(latitude: params[:pos]).each do |poi|
+      @user = {}
+      poi_id = poi.id
+      poi.journeys.each do |journey|
+        contenu = journey.journey_pois.where(poi_id: poi_id).first
+        @user = {
+          email: journey.user.email,
+          journeyname: journey.name,
+          poiname: contenu.name,
+          poiphoto: contenu.photo.url,
+          poivideo: contenu.video,
+          poitext: contenu.text,
+          poisound: contenu.sound,
+
+        }
+        @result << @user
+      end
+    end
+    # @a = {lato: params[:pos]}
+    render :json => @result
   end
 end
