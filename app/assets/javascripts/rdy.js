3eaefadda5eb117971683800a85162372e46ac5c
Romain DESMIDT <romain.desmidt@gmail.com>
2017-03-29 01:15:31 +0200

    small changes second iteration

--- app/assets/javascripts/rdy.js
+++ app/assets/javascripts/rdy.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".tabs-menu a").click(function(event) {
+    $(".tabs-menu-index a").click(function(event) {
         event.preventDefault();
         var classToRem = $(this).attr('class');
         var h4ToChange = $('h4.' + classToRem);
@@ -8,8 +8,11 @@ $(document).ready(function() {
         $(this).parent().siblings().removeClass("current");
         $('h4').removeClass("hidden-content");
         h4ToChange.siblings().addClass("hidden-content");
-        $('div.tab > div').attr('style', '');
-        divToChange.siblings().attr('style', 'display: none;');
+
+        $('div.tab > div').removeClass("hidden-content");
+        divToChange.siblings().addClass("hidden-content");
+        // $('div.tab > div').attr('style', '');
+        // divToChange.siblings().attr('style', 'display: none;');
     });
 });
 
