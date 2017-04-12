e89be21e94d470aad678980602f91e89844cc454
Romain DESMIDT <romain.desmidt@gmail.com>
2017-04-12 22:00:41 +0200

    journeys index test

--- test/integration/journeys_index_test.rb
+++ test/integration/journeys_index_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class JourneysIndexTest < ActionDispatch::IntegrationTest
-  test "home loads correctly" do
+  test "journeys loads correctly" do
     visit "/journeys"
-    save_and_open_screenshot
+    #save_and_open_screenshot
     assert_equal 200, page.status_code
  end
+
 end
