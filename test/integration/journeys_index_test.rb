9308a217f5440bc6c527a0edd028cd2ebeb2930d
Romain DESMIDT <romain.desmidt@gmail.com>
2017-04-15 20:22:12 +0200

    more integration test

--- test/integration/journeys_index_test.rb
+++ test/integration/journeys_index_test.rb
@@ -4,9 +4,14 @@ class IntegrationJourneysIndexTest < ActionDispatch::IntegrationTest
   def setup
     @journeys= journey_pois(:jpone)
     @poi = pois(:poione)
+    @arr_journey = []
+    @arr_journey << @journeys
   end
 
 # could test on presence of error message that could be written in dom by js
+# error to check => not as many marker as in db journey_poi
+# error to ajax call
+# error when clicking on profil face, should change content
 
   test "journeys index loads correctly and put a marker on the map" do
     Capybara.current_driver = :selenium
@@ -20,4 +25,10 @@ class IntegrationJourneysIndexTest < ActionDispatch::IntegrationTest
     #p page.status_code
     #assert_equal 200, page.status_code
  end
+
+  test "journeys POI all should be the same as all markers length" do
+    Capybara.current_driver = :selenium
+    visit "/journeys"
+    assert (page.evaluate_script("Object.keys(all_markers).length") == @arr_journey.length), " all_markers.length = #{page.evaluate_script('Object.keys(all_markers).length')} should be equal to journey_pois.length got #{@arr_journey.length}"
+  end
 end
